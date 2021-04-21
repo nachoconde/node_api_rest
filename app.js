ea28a7222bb4398516205e18afb080a38c0da962
Nacho Conde <nachok@gmail.com>
2021-04-21 10:14:33 +0200

    añadida validación y método post

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-const express = require ('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express ();
-const feedRoutes = require ('./routes/feed')
+const app = express();
+const feedRoutes = require('./routes/feed')
+const mongoose = require('mongoose')
+
+const MONGO_URI = 'mongodb+srv://jfY8xA9DEGFlabL9:jfY8xA9DEGFlabL9@cluster0.spi4x.mongodb.net/feeds'
 
 //https://medium.com/@mmajdanski/express-body-parser-and-why-may-not-need-it-335803cd048c
 app.use(express.json());
 
 
-app.use ( (req, res, next ) => {
-    console.log ('headers');
-    
-    res.setHeader ('Access-Control-Allow-Origin', '*') ; //admitimos cualquier cliente
-	res.setHeader ('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE' );
-	res.setHeader ('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next ();
+app.use((req, res, next) => {
+    console.log('headers');
+
+    res.setHeader('Access-Control-Allow-Origin', '*'); //admitimos cualquier cliente
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 
 })
-app.use ('/feed',feedRoutes);
+app.use('/feed', feedRoutes);
 
-app.listen (8080);
+mongoose
+.connect(MONGO_URI)
+    .then(sucess => {
+        app.listen(8080);
+
+    })
+    .catch(err => { 
+        console.log (err);
+    })
 
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,24 +1,53 @@
+const { validationResult } = require('express-validator')
+const Post = require ('../modes')
+
 exports.getPost = (req, res, next) => {
-    console.log('feed/post');
-    res.status(200).json( {
-        posts:[
-            {title: 'Prueba de titulo',
-             content: 'este es el contenido',
-             imageUrl: 'images/crossfit.jpg'  }
+
+    console.log('GET:feed/post');
+    res.status(200).json({
+        posts: [
+            {
+                title: 'Prueba de titulo',
+                _id: '111',
+                author: "nacho",
+                content: 'este es el contenido',
+                imageUrl: 'images/crossfit.jpg',
+                creator: {
+                    name: "nacho",
+                },
+                createdAt: new Date()
+            }
         ]
-    }) 
+    })
 };
 
 
 //cada post title, author, image, content
 
 exports.postPost = (req, res, next) => {
+    console.log('POST:feed/post');
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array())
+        return res.status(422)
+            .json({
+                message: 'Validation failed',
+                errors: errors.array()
+            })
+    }
+
     const title = req.body.title;
     const content = req.body.content;
 
     res.status(201).json({
-        message: 'post created',
-        post: { title: title, content: content }
-
+        message: 'created',
+        post: {
+            _id: new Date().toISOString(),
+            title: title,
+            content: content,
+            creator: { name: 'nacho' },
+            createdAt: new Date()
+        }
     })
 };
